a8c67b8cc4732557a6cf8f4a7ab4ac68981a437d
GolfGrab <surapus01@gmail.com>
2023-08-24 16:04:49 +0700

    refactor

--- script.js
+++ script.js
@@ -1,68 +1,111 @@
-let todoList = localStorage.getItem("todoList")
-  ? JSON.parse(localStorage.getItem("todoList"))
-  : [];
+/*
 
-const handleAddTodo = (e) => {
-  e.preventDefault();
-  const todoInput = document.querySelector("#todo-input");
-  const todo = todoInput.value;
-  if (todo.trim().length > 0) {
-    const todoObj = {
-      id: todo.replace(" ", "-"),
-      todo: todo,
-      isDone: false,
-    };
-    todoList.push(todoObj);
-    localStorage.setItem("todoList", JSON.stringify(todoList));
-  }
-  render();
-
-  document.querySelector("#todo-input").value = "";
-};
-
-const handleDeleteTodo = (e) => {
-  const todoId = e.target.id;
-  todoList = todoList.filter((todo) => todo.id !== todoId);
-  localStorage.setItem("todoList", JSON.stringify(todoList));
-  render();
-};
-
-const handleCheckbox = (e) => {
-  const todoId = e.target.id;
-  todoList.forEach((todo) => {
-    if (todo.id === todoId) {
-      todo.isDone = !todo.isDone;
+type Todo = {
+    id: string, // random string
+    name: string,
+    isDone: boolean,
     }
+
+ type Todos = Todo[]
+
+*/
+
+let todos = [
+  {
+    id: "1",
+    name: "Learn HTML",
+    isDone: true,
+  },
+  {
+    id: "2",
+    name: "Learn CSS",
+    isDone: true,
+  },
+  {
+    id: "3",
+    name: "Learn JS",
+    isDone: false,
+  },
+];
+
+init();
+
+function init() {
+  loadTodos();
+  renderTodoList();
+}
+
+function TodoComponent(todo) {
+  const { id, name, isDone } = todo;
+
+  return /*html*/ `
+    <li class="todo-item">
+      <input
+        type="checkbox"
+        id=${id}
+        ${isDone ? "checked" : ""}
+        onclick="toggleDone('${id}')"
+      />
+      <label
+        for="${id}"
+        class="${isDone ? "checked" : ""}"
+      >
+        ${name}
+      </label>
+      <button id=${id} onclick="deleteTodo('${id}')">
+        Delete
+      </button>
+    </li>
+  `;
+}
+
+function renderTodoList() {
+  const todoList = document.querySelector(".todo-list");
+  todoList.innerHTML = "";
+
+  todos.forEach((todo) => {
+    todoList.innerHTML += TodoComponent(todo);
   });
-  localStorage.setItem("todoList", JSON.stringify(todoList));
-  render();
-};
+}
 
-const render = () => {
-  const todoListLength = todoList.length;
-  const todoListElement = document.querySelector("#todo-list");
-  todoListElement.innerHTML = "";
-  for (let i = 0; i < todoListLength; i++) {
-    const { id, todo, isDone } = todoList[i];
-    const newTodoItem = /*html*/ `
-      <li class="todo-list-item">
-        <input
-          type="checkbox"
-          id=${id}
-          ${isDone ? "checked" : ""}
-          onclick="handleCheckbox(event)"
-        />
-        <label
-          for="${id}"
-          class="${isDone ? "label-checked" : ""}"
-        >
-          ${todo}
-        </label>
-        <button id=${id} onclick="handleDeleteTodo(event)">Delete</button>
-      </li>
-    `;
-    todoListElement.insertAdjacentHTML("beforeend", newTodoItem);
+function addTodo(event) {
+  event.preventDefault();
+  todos.push({
+    id: Math.random().toString(),
+    name: event.target.todo.value,
+    isDone: false,
+  });
+  event.target.todo.value = "";
+  renderTodoList();
+  saveTodos();
+}
+
+function toggleDone(id) {
+  todos = todos.map((todo) => {
+    if (todo.id === id) {
+      return {
+        ...todo,
+        isDone: !todo.isDone,
+      };
+    }
+    return todo;
+  });
+  renderTodoList();
+  saveTodos();
+}
+
+function deleteTodo(id) {
+  todos = todos.filter((todo) => todo.id !== id);
+  renderTodoList();
+  saveTodos();
+}
+
+function loadTodos() {
+  if (localStorage.getItem("todos")) {
+    todos = JSON.parse(localStorage.getItem("todos"));
   }
-};
+}
 
-render(todoList);
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
